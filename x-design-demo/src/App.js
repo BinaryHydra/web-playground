3ea7cdd7ce648662adb23767318e825668c70126
mantas.p <man.petrikas@gmail.com>
2018-01-02 21:05:15 +0200

    redit using css borders. Now need to fix border z indexes

--- x-design-demo/src/App.js
+++ x-design-demo/src/App.js
@@ -5,8 +5,15 @@ class App extends Component {
   render() {
     return (
       <div className="centered">
-        <div className="square top"/>
-        <div className="square bottom"/>
+        <div className="triangle top hoverable">
+          <div  className="inside top">
+            <label>top</label>
+            <button>button</button>
+            </div>
+        </div>
+        <div className="triangle left"/>
+        <div className="triangle bottom"/>
+        <div className="triangle right"/>
       </div>
     );
   }
